f1f602e6b6f067845d22dd1742c8a94c22a8b51e
rabboh <rabboh@yahoo.com>
2023-12-17 18:42:30 +0100

    more led instances

--- ledoop/ledoop.ino
+++ ledoop/ledoop.ino
@@ -1,21 +1,34 @@
 #include "led.h"
 
-Led myled(13);
+Led myredled(13);
+Led myyellowled(12);
+Led mydarkgreenled(11);
+Led mylightgreenled(10);
+
 int i;
 int delayTiming;
 char control[] = "LLLLHHHLHLHHLHLHLHLLLHLLHLHLLHLLHLLLHLLLHLLHLHLLHLLLHLLHLLHLLHLLHLLHLLHHHHHLLHHHHHHLHLLLHLLHLLH";
 
 void setup() {
   pinMode(13, OUTPUT);
-  delayTiming = 100;
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  delayTiming = 1000;
 }
 
 void loop() {
   for (i = 0; i < strlen(control); i++) {
     if (control[i] == 'H') {
-      myled.on();
+      myredled.on();
+      myyellowled.on();
+      mydarkgreenled.on();
+      mylightgreenled.on();
     } else {
-      myled.off();
+      myredled.off();
+      myyellowled.off();
+      mydarkgreenled.off();
+      mylightgreenled.off();
     }
     delay(delayTiming);
   }
